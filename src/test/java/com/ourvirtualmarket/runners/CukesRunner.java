3df4433426fbe48998eb95b17391ea453d235546
busra <busrahyt42@gmail.com>
2023-06-12 20:01:15 +0300

    Login_StepDefs has been updated

--- src/test/java/com/ourvirtualmarket/runners/CukesRunner.java
+++ src/test/java/com/ourvirtualmarket/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/ourvirtualmarket/steps_defs",
-        dryRun = false,
+        dryRun = true,
         tags = "@wip"
 )
 public class CukesRunner {
