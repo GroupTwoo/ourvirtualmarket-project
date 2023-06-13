8a8da79327c4a14540c6e3fc094e194142319014
busra <busrahyt42@gmail.com>
2023-06-13 20:29:18 +0300

    New scenario steps added in addToCart.feature

--- src/test/java/com/ourvirtualmarket/runners/CukesRunner.java
+++ src/test/java/com/ourvirtualmarket/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/ourvirtualmarket/steps_defs",
-        dryRun = true,
+        dryRun = false,
         tags = "@wip"
 )
 public class CukesRunner {
